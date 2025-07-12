b8628ca8027dfdbbbcaf96c468b32dfbec72ed35
NADEEN-cash <wadadannena@tutamail.com>
2025-07-12 11:24:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'XVUGkZAJ#9s5arXLTJfvV-KSux8oYUFgldUU6f8bmLBPAeNqhPEM' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '6BdHGaAL#3-c09aBlIBJ51SMZjU7ZjpOfbiZD2wjkeRSN7e_bTmA' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
